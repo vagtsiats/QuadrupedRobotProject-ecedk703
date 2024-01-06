787f8bb1290d241930cc95e393119dd37d2cf32a
NtagkasAlex <up1083874@upnet.gr>
2024-01-06 17:42:14 +0200

    Added Example

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
 #include <Arduino.h>
+#include <ArduinoEigenDense.h>
+using namespace Eigen;
 
 // put function declarations here:
-int myFunction(int, int);
+void printArray(MatrixXd a);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+  Serial.begin(9600);
 
+    MatrixXd matrix  {3, 3};
+
+    VectorXd vec{3};
+    vec << 1, 2, 3;
+    //Create Matrix from collumn vectors
+    matrix << vec, vec, vec;
+  //Custom print Function
+  printArray(matrix);
+
+}
 void loop() {
   // put your main code here, to run repeatedly:
 }
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void printArray(MatrixXd a){
+   for (int i = 0; i < a.rows(); i++) {
+        for (int j = 0; j < a.cols(); j++) {
+      Serial.print(a(i,j));
+      Serial.print("\t");
+    }
+    Serial.println();
+  } 
 }
