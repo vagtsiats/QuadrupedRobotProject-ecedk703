ae372c92454f1c947b6ee1ba98c248c949a32ed7
NtagkasAlex <up1083874@upnet.gr>
2024-02-14 21:59:02 +0200

    ComReady

--- src/definitions.h
+++ src/definitions.h
@@ -12,9 +12,54 @@ using namespace BLA;
 const u_int32_t LOOP_PERIOD = 1e4; // 0.01sec
 u_int32_t timer0;                  // constant loop timer
 double timer1;                     // general purpose timer
+#define MAX_INPUT 10
 
 Quad Robot;
 
+void processData (const char * data)
+  {
+    //if first element of data is digit or -
+    if(isdigit(*data) || data[0]=='-'){
+        float walkSpeed = std::stof(data);
+        Serial.println(walkSpeed);
+    }
+    else if (*data=='r' || *data=='R') {
+        //Action for turning right 
+        Serial.println("Turn Right");
+    }
+    else if (*data=='l' || *data=='L') {
+        //Action for turning left 
+        Serial.println("Turn left");
+    }
+    else if (*data=='s' || *data=='S') {
+        //Action for Stand
+        Serial.println("Stand");
+    }
+    else{
+        Serial.println("Non Valid Action");
+    }
+  }  
+  
+void processIncomingByte (const byte inByte)
+  {
+  static char input_line [MAX_INPUT];
+  static unsigned int input_pos = 0;
+
+  if(inByte=='\n'){
+    input_line [input_pos] = 0;  
+        
+        // '\n' -> End of line
+        process_data (input_line);
+        
+        // reset buffer for next time
+        input_pos = 0; 
+  }  
+  else{
+    if (input_pos < (MAX_INPUT - 1))
+        input_line [input_pos++] = inByte;
+  }
+
+}  
 // Matrix<3> theta0 = {0, M_PI/6,-M_PI/4};
 // float t;
 
