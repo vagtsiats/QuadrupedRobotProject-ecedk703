c33516e873e7453b7ecbb03f0972f1b85171b7b6
VagTsiats <vagtsiats12@gmail.com>
2024-03-20 23:08:33 +0200

    cleaned main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,17 @@
 #include "definitions.h"
 using namespace BLA;
 
-Trajectory leg_traj(4, -14);
-Turn turning(4, -14);
 void setup()
 {
     Serial.begin(9600);
 
     digitalWrite(LED_BUILTIN, 1);
 
-    // NOTE - trajectory test
-    // Trajectory leg_traj(1, 4, -14);
-    // delay(2000);
-    // double period = leg_traj.get_T();
-
-    // for (double t = 0; t < 2 * period; t = t + 0.1)
-    // {
-    //     BLAprintMatrix(leg_traj.get_position(fmod(t, period)));
-    // }
     Robot.initHardware();
-    turning.calculateTurnParameters();
 
     timer0 = micros();
 }
 
 void loop()
 {
-    // SECTION - Alex Code
-    float t = micros() - timer0; // in micros
-    float t_sec = t / 1e6;
-    double period = leg_traj.get_T();
-    //  Matrix<3> doubleMatrix1=leg_traj.get_position(fmod(t_sec, period));
-    //  Matrix<3> doubleMatrix2= leg_traj.get_velocity(fmod(t_sec, period));
-    //  Matrix<3> xd,xd_dot;
-
-    // xd(0)=-doubleMatrix1(2);
-    // xd(1)=doubleMatrix1(1);
-    // xd(2)=doubleMatrix1(0);
-
-    // xd_dot(0) = -doubleMatrix2(2);
-    // xd_dot(1) = doubleMatrix2(1);
-    // xd_dot(2) = doubleMatrix2(0);
-
-    // Matrix<3> theta0=Robot.br.InverseKinematics({14,0,2});
-    // // BLAprintMatrix(xd);
-    // Robot.br.updateTranslations(theta0);
-
-    // Robot.br.inverseDiffKinematics(theta0, xd,xd_dot);
-    delay(20);
-
-    //! SECTION
-
-    // SECTION - constant time loop :
-    if (micros() - timer0 >= LOOP_PERIODms)
-    {
-        timer0 = micros();
-        timer1 = timer0 / 1e6;
-
-        // Robot.gait(timer1);
-        // NOTE - DiffKinematics Test
-        // Robot.br.inverseDiffKinematics({0, 0, 0}, {3, 0, -10}, {0, 0, 0});
-
-        Robot.br.JTransIK({3, 0, -10}, BLAdiagonal<3>(0.1), LOOP_PERIODsec);
-
-        Serial.println();
-    }
-
-    // BLAprintMatrix(BLAdiagonal<3>(1));
-    // Serial.println();
 }
