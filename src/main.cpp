20b6c6aec181e9f5d1271cdff10a10543fbd046d
ChristosK17 <christoskiokakis@gmail.com>
2024-05-18 14:52:29 +0300

    Implemented Wifi communication
    The Arduino connects to a network. The original thought was to function as an AP, but this would lower the performance. The calculations to follow trajectories are really complex and occupy a lot of resources.

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,74 @@
+#include <SPI.h>
+#include <WiFiNINA.h>
 #include "definitions.h"
 using namespace BLA;
-#define TEST
-void setup()
-{
+
+// #define TEST
+
+const char* ssid = "wifiSSID";
+const char* password = "wifiPass";
+
+WiFiServer server(23);
+
+unsigned long timer0;
+float SPEED = 1.0;
+
+void setup() {
     Serial.begin(9600);
+    while (!Serial);
+
+    if (WiFi.status() == WL_NO_MODULE) {
+        Serial.println("No WiFi module found!");
+        while (true);
+    }
+
+    int status = WiFi.begin(ssid, password);
+    while (status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to SSID: ");
+        Serial.println(ssid);
+        delay(10000);
+        status = WiFi.begin(ssid, password);
+    }
+
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    server.begin();
 
     digitalWrite(LED_BUILTIN, 1);
+    Robot.initHardware();
+    Robot.init_trot(2);
 
-    // initial delay before start
     delay(2000);
 
-    Robot.initHardware();
-    Robot.init_walk(1);
-
-    digitalWrite(LED_BUILTIN, 1);
-    delay(1000);
-
-    timer_0 = micros();
+    timer0 = micros();
 }
 
-void loop()
-{
-
+void loop() {
 #ifdef TEST
-    // Robot.drive_legs({0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0});
-
-    Robot.drive_legs_IK(pos, pos, pos, pos);
-
+    // Robot.drive_legs(conf, conf, conf, conf);
+    // Robot.drive_legs_IK(pos, pos, pos, pos);
 #else
-    // SECTION - constant time loop :
-    if (micros() - timer0 >= LOOP_PERIODus)
-    {
+    if (server.available()) { // If there is a client, read data
+        while (client.connected()) {
+            if (client.available()) {
+                String input = client.readStringUntil('\n');
+                Serial.println("Received: " + input);
+                SPEED = input.toFloat();
+                Serial.print("Updated SPEED: ");
+                Serial.println(SPEED);
+            }
+        }
+        client.stop();
+    }
+    
+    if (micros() - timer0 >= LOOP_PERIODus) {
         timer0 = micros();
 
         Robot.walk(micros() / 1.e6, LOOP_PERIODsec);
+        Robot.speed(SPEED);
     }
-    ///!SECTION
 #endif
 }
