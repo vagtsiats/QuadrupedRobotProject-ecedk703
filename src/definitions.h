bcd7f8290b0975a3f844f1af0f6c0aee8c78d4a9
VagTsiats <vagtsiats12@gmail.com>
2024-05-21 16:50:22 +0300

    wifi integration

--- src/definitions.h
+++ src/definitions.h
@@ -6,16 +6,27 @@
 #include "BLA_tools.h"
 #include "trajectory.h"
 #include "Quad.h"
+#include <SPI.h>
+#include <WiFiNINA.h>
 
 using namespace BLA;
 
 const uint32_t LOOP_PERIODus = 10; // 0.0001sec
 const float LOOP_PERIODsec = LOOP_PERIODus / 1e6;
-uint32_t timer0; // constant loop timer
-uint32_t timer1; // general purpose timer used for trajectory
-uint32_t timer2; // init algorithm timer
+uint32_t timer_0; // constant loop timer
+uint32_t timer1;  // general purpose timer used for trajectory
+uint32_t timer2;  // init algorithm timer
 
 Matrix<3> conf = {0, -0.5, 1};
 Matrix<3> pos = {0, 0, -20};
 
 Quad Robot;
+
+const char *ssid = "Xiaomi 12";
+const char *password = "paparia2018";
+
+WiFiServer server(23);
+WiFiClient client;
+
+float SPEED = 2.0;
+bool Stand = true;
--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,85 @@
 #include "definitions.h"
 using namespace BLA;
+
 // #define TEST
+
 void setup()
 {
     Serial.begin(9600);
+    while (!Serial)
+        ;
+
+    // if (WiFi.status() == WL_NO_MODULE) {
+    //     Serial.println("No WiFi module found!");
+    //     while (true);
+    // }
+
+    int status = WiFi.begin(ssid, password);
+    while (status != WL_CONNECTED)
+    {
+        Serial.print("Attempting to connect to SSID: ");
+        Serial.println(ssid);
+        delay(10000);
+        status = WiFi.begin(ssid, password);
+    }
+
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    server.begin();
 
     digitalWrite(LED_BUILTIN, 1);
-
-    // initial delay before start
-    // delay(2000);
-
     Robot.initHardware();
+    Robot.init_trot(SPEED);
+
+    delay(2000);
+
+    timer_0 = micros();
+
     // Robot.drive_legs(conf, conf, conf, conf);
     // Robot.fr.InverseKinematics({10, 5, -20});
     // Robot.fl.InverseKinematics({10, 5, -20});
-
-    Robot.init_trot(2);
 
     // Robot.drive_legs();
     // Robot.init_walk(1);
     // BLAprintMatrix(Robot.fr.getEndEffectorPosition());
     delay(2000);
 
-    timer0 = micros();
+    timer_0 = micros();
 }
 
 void loop()
 {
-
 #ifdef TEST
     // Robot.drive_legs(conf, conf, conf, conf);
-
     // Robot.drive_legs_IK(pos, pos, pos, pos);
-
 #else
-    // SECTION - constant time loop :
-    if (micros() - timer0 >= LOOP_PERIODus)
-    {
+    client = server.available();
+    if (client)
+    { // If there is a client, read data
+        while (client.connected())
+        {
+            if (client.available())
+            {
+                String input = client.readStringUntil('\n');
+                // Serial.println("Received: " + input);
+                SPEED = input.toFloat();
+                // Serial.print("Updated SPEED: ");
+                Serial.println(SPEED);
+                Robot.init_trot(SPEED);
+            }
 
-        timer0 = micros();
-        // Serial.println(timer0 / 1.e6);
+            if (micros() - timer_0 >= LOOP_PERIODus)
+            {
+                timer_0 = micros();
 
-        Robot.walk(micros() / 1.e6, LOOP_PERIODsec);
+                Robot.walk(micros() / 1.e6, LOOP_PERIODsec);
+            }
+        }
+        client.stop();
     }
-    ///!SECTION
+
 #endif
 }
